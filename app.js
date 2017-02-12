c31558c126d3254a80763df0930bd575565efaa9
feilong.cao <feilong.cao@fengjr.com>
2017-02-12 23:11:28 +0800

    fox

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var api = require('./api');
 var Router = require('koa-router');
 var router = new Router();
 var config = require('config');
-//var wechat = require('co-wechat')(config.weixin);
-//var weixin = require('./weixin/weixinStore.js');
+var wechat = require('co-wechat')(config.weixin);
+var weixin = require('./weixin/weixinStore.js');
 
 require('./mongoose/Connection.js');
 
@@ -13,10 +13,15 @@ var bodyParser = require('koa-bodyparser');
 
 router.use('/api', api.routes());
 
+
+//router.get('/weixin', (ctx) => {wechat.middleware(weixin.event).bind(ctx)()});
+
 var app = new koa();
 
 app.use(bodyParser());
 
+
 app.use(router.routes());
+
 
 app.listen(3000);
