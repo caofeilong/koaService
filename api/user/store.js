49dada207a0ad27cb3b8a289f5088baf0f25f4a2
feilong.cao <feilong.cao@fengjr.com>
2017-02-19 11:04:33 +0800

    fix

--- api/user/store.js
+++ api/user/store.js
@@ -4,3 +4,15 @@ var User = require('../../mongoose/schemas/User.js')
 exports.saveUser = function (userInfo) {
     return User.create(userInfo)
 };
+
+
+exports.userList = function () {
+    return User.find().select('_id uname pwd');
+};
+
+
+exports.delete = function (_id) {
+    return User.remove({_id});
+};
+
+
--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var bodyParser = require('koa-bodyparser');
 
 router.use('/api', api.routes());
 
+//
+//router.get('/weixin', function (ctx, next) {
+//    var g = wechat.middleware(weixin.event).bind(ctx, next)();
+//    g.next()
+//});
+
 var app = new koa();
 app.use(bodyParser());
 app.use(router.routes());
